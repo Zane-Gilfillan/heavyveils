853a3c26b5d77c0f7a18a9194fe4101386f455ad
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-08-05 18:08:22 -0500

    installed countdown. need to style

--- components/Showcase.js
+++ components/Showcase.js
@@ -1,6 +1,20 @@
 import styles from '../styles/Showcase.module.scss'
 import Image from 'next/image'
 import art from '../public/img/4.jpg'
+import Countdown from 'react-countdown';
+
+
+const Completionist = () => <span>lets jam</span>;
+
+const renderer = ({ hours, minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a completed state
+      return <Completionist />;
+    } else {
+      // Render a countdown
+      return <span>{hours}:{minutes}:{seconds}</span>;
+    }
+  };
 
 const Showcase = () => {
     return (
@@ -8,11 +22,23 @@ const Showcase = () => {
             
             <div className={styles.container}>
                 
-                <div className={styles.text_wrap}>
-                    <h1>permahaze</h1>
-                </div>
+                <div className={styles.main}>
+                    <div className={styles.text_wrap}>
+                        <h1>permahaze</h1>
+                    </div>
                         
-                <Image src={art} height='700' width='700' />
+                    <Image src={art} height='600' width='600' />
+                </div>
+                
+                <div className={styles.countdown}>
+
+                <Countdown
+                    
+                    date={Date.now() + 150000000}
+                    renderer={renderer}
+                />
+
+                </div>
                 
             </div>
             
