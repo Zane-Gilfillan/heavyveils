aa119e1d169c9d57d21e7402b68d6e4bc66992f1
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-09-13 17:52:34 -0500

    media issue try

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ import Showcase from '../components/Showcase.js'
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+      </Head>
       <Nav />
       <Showcase />
     </>
