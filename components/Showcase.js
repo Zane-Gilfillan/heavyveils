54ad1e415268265a7a5ff8a96dce4c7adc0393e2
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-09-13 16:31:38 -0500

    import b_c

--- components/Showcase.js
+++ components/Showcase.js
@@ -1,6 +1,8 @@
+import Head from 'next/head'
 import styles from '../styles/Showcase.module.scss'
 import Image from 'next/image'
 import art from '../public/img/4.jpg'
+import wall from '../public/img/wall.jpg'
 
 
 
@@ -8,21 +10,27 @@ import art from '../public/img/4.jpg'
 const Showcase = () => {
     return (
         <>
-            
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+                <link href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo&display=swap" rel="stylesheet"></link>
+            </Head>
             <div className={styles.container}>
                 
                 <div className={styles.main}>
                     <div className={styles.text_wrap}>
                        <h1>melvin henry</h1>
+                       <h2>the proscenium crash suite</h2>
                     </div>
 
                     <div className={styles.img_wrap}>
-                        <Image src={art} height='800' width='800' />
+                        <Image src={wall} height='600' width='1000' />
+                    </div>
+
+                    <div className={styles.audio_wrap}>
+                        <iframe className={styles.bc_player} src="https://bandcamp.com/EmbeddedPlayer/album=2810766443/size=small/bgcol=333333/linkcol=ffffff/transparent=true/" seamless><a href="https://heavyveils.bandcamp.com/album/proscenium-crash">Proscenium Crash by Melvin Henry</a></iframe>
                     </div>
                     
-                    <div className={styles.text_wrap}>
-                       <h1>proscenium crash</h1>
-                    </div>
                 </div>
                 
             </div>
