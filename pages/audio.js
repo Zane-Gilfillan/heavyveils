0095ba9e10e6adfb8bd408ca356a2277c5084d7b
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-05-17 15:29:24 -0500

    updated all style sheets w/ media queries

--- pages/audio.js
+++ pages/audio.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Nav from '../components/Nav'
 import Mini from '../components/mini_nav'
 import styles from '../styles/Audio.module.scss'
-import wall from '../public/img/wall.jpg'
+import wall from '../public/img/wall_ws_edit.jpg'
 import smile from '../public/img/smile_resize.jpg'
 
 const audio = () => {
@@ -12,11 +12,11 @@ const audio = () => {
         <Nav />
         <div className={styles.container}>
             <div className={styles.album__card}>
-              <div className={styles.img__holder}><Image src={smile} /></div>
+              <div className={styles.img__holder}><Image className={styles.img} src={smile} /></div>
               <iframe className={styles.album__info} src="https://bandcamp.com/EmbeddedPlayer/album=3993573502/size=large/bgcol=333333/linkcol=ffffff/artwork=none/transparent=true/" seamless><a href="https://heavyveils.bandcamp.com/album/death-knell-relics-mix">Death Knell Relics Mix by Melvin Henry</a></iframe>
             </div>
             <div className={styles.album__card}>
-              <div className={styles.img__holder}><Image src={wall} /></div>
+              <div className={styles.img__holder}><Image className={styles.img} src={wall} /></div>
               <iframe className={styles.album__info} src="https://bandcamp.com/EmbeddedPlayer/album=2810766443/size=large/bgcol=333333/linkcol=ffffff/artwork=none/transparent=true/" seamless><a href="https://heavyveils.bandcamp.com/album/proscenium-crash">Proscenium Crash by Melvin Henry</a></iframe>
             </div>
         </div>
--- pages/other.js
+++ pages/other.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import styles from '../styles/Other.module.scss'
+import Nav from '../components/Nav'
+
+
 
 const other = () => {
   return (
     <>
-        <h1>other</h1>
+        <Nav />
+        <div className={styles.container}>
+          <div className={styles.vid__holder}>
+            <video className={styles.vid} src='/vid/crash_cut.mp4' type='video/mp4' autoPlay />
+          </div>
+          
+        </div>
     </>
   )
 }
